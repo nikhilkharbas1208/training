15b0bdb0779faeaccf8b81ff762e83e0caa03615
Vaishnavi Shinde <vaishnavis@prorigo.com>
2025-06-06 18:23:52 +0530

    React Router

--- react_router_v6/src/Components/UI/UserDetailPage.jsx
+++ react_router_v6/src/Components/UI/UserDetailPage.jsx
@@ -1,7 +1,11 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 export const UserDetailPage = () => {
     const params = useParams();
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1);
+    }
     console.log(params);
     const userData =useLoaderData();
     console.log(userData);
@@ -13,7 +17,9 @@ export const UserDetailPage = () => {
         <h4>email:{email}</h4>
         <h4>phone:{phone}</h4>
         <h4>Address: {JSON.stringify(address)}</h4>
-
+        <button onClick={ handleGoBack }>
+            Go Back
+        </button>
         </>
     );
 };
