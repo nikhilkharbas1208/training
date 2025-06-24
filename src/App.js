855602eddbaefa077498a55ed6ceee12697d6499
Vaishnavi Shinde <vaishnavis@prorigo.com>
2025-06-24 12:21:21 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import './App.css';
-// import { State } from './Components/State';
-// import { DerivedState } from './Components/DerivedState';
-//import { MessageProps } from './Components/MessageProps';
-import { BioProvider } from './hooks/ContextAPI';
-import { Home } from './hooks/ContextAPI/Home';
-import { About } from './hooks/ContextAPI/About';
-//import { ParentComponent } from './Components/PropDrilling';
+import React, { Suspense, lazy } from 'react';
+import { Provider } from 'react-redux';
+import { Store } from './store/Store';
+import JiraTable from './components/JiraTable';
+import { JiraIssuesProvider } from './context/JiraIssuesContext';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './components/ErrorBoundry';
+import Skeleton from 'react-loading-skeleton';
+import "react-loading-skeleton/dist/skeleton.css";
+
+const JiraTableTest = lazy(() => import('./components/JiraTableTest'));
+const IssueDetailsPage = lazy(() => import('./components/IssueDetailsPage'));
+const CreateIssuePage = lazy(() => import('./components/CreateIssuePage'));
+// const ErrorBoundary = ({ children }) => {
+//   return (
+//     <Suspense fallback={<p>Loading...</p>}>
+//       {children}
+//     </Suspense>
+//   );
+// };
+
 function App() {
   return (
     <div className="App">
-      {/* <State/> */}
-      {/* <DerivedState/> */}
-      {/* <MessageProps/> */}
-      {/* <ParentComponent/> */}
-      <BioProvider>
-        <Home/>
-        <About/>
-      </BioProvider>
+    {/* <Provider store={Store}>
+    <JiraTable projectKey="PRAC" />
+    </Provider> */}
+     <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <BrowserRouter>
+      <JiraIssuesProvider projectKey="PRAC">
+        <Suspense fallback={<div><Skeleton height={30} width={200} style={{ marginBottom: 10 }} />
+        <Skeleton height={20} count={5} style={{ marginBottom: 6 }} /></div>}>
+         <Routes>
+          <Route path="/" element={<JiraTableTest />} />
+          <Route path="/issue/:issueId" element={<IssueDetailsPage />} />
+          <Route path="/create" element={<CreateIssuePage />} />
+        </Routes>
+        </Suspense>
+      </JiraIssuesProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
     </div>
   );
 }
