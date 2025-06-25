f1624712f4256f25fa40008a8aeb8d2491e38beb
vaishnavishinde4 <vaishnavi.shinde@prorigosoftware.com>
2025-06-25 14:27:07 +0530

    Delete redux-app directory

--- redux-app/src/store.jsx
+++
@@ -1,9 +0,0 @@
-
-import { configureStore } from '@reduxjs/toolkit';
-import { taskReducer } from './features/tasks/taskSlice';
-
-export const store = configureStore({
-    reducer:{
-        taskReducer: taskReducer.reducer,
-    },
-})
