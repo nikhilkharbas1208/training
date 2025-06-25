ddc99ed59cbb879db07ef40d91b8c5e62ef19ea0
vaishnavishinde4 <vaishnavi.shinde@prorigosoftware.com>
2025-06-25 14:48:04 +0530

    Delete Assignment directory

--- Assignment/assignment/src/reducers/Store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import tableReducer from '../features/tableSlice';
-
-
-export const Store = configureStore({
-    reducer : {
-        table : tableReducer,
-    },
-})
