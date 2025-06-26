7a524b0f1788954296374ff779b4154542fc9f9b
Vaishnavi Shinde <vaishnavis@prorigo.com>
2025-06-26 11:18:38 +0530

    Implemented common loader

--- src/Components/common/LoaderComponent.jsx
+++ src/Components/common/LoaderComponent.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Loader.module.css';
+import { ClipLoader } from 'react-spinners';
 
 const LoaderComponent = ({ message = "Loading..." }) => {
   return (
     <div className={styles.loaderOverlay}>
-      <div className={styles.spinner}></div>
+      <ClipLoader color="#007bff" size={50} />
       <div className={styles.loadingMessage}>{message}</div>
     </div>
   );
 };
+
 export default LoaderComponent;
